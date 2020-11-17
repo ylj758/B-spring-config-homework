a92f4b20d213eab2abc9e3fe8ff3c7e0c2112fc4
ylj758 <758766951@qq.com>
2020-11-17 22:16:31 +0800

    [ylj]: implement Problem1

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import java.util.List;
+import java.util.Map;
 
 @ConfigurationProperties("mail")
 public class MailConfig {
@@ -8,6 +10,21 @@ public class MailConfig {
     private String hostname;
     private int port;
     private String from;
+    private List<String> defaultRecipients;
+    private Map<String, String> additionalHeaders;
+    private Credentials credentials;
+
+    @Override
+    public String toString() {
+        return "MailConfig{" +
+                "hostname='" + hostname + '\'' +
+                ", port=" + port +
+                ", from='" + from + '\'' +
+                ", defaultRecipients=" + defaultRecipients +
+                ", additionalHeaders=" + additionalHeaders +
+                ", credentials=" + credentials +
+                '}';
+    }
 
     public String getHostname() {
         return hostname;
@@ -33,12 +50,27 @@ public class MailConfig {
         this.from = from;
     }
 
-    @Override
-    public String toString() {
-        return "MailConfig{" +
-                "hostname='" + hostname + '\'' +
-                ", port=" + port +
-                ", from='" + from + '\'' +
-                '}';
+    public List<String> getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(List<String> defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public Map<String, String> getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAdditionalHeaders(Map<String, String> additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
     }
 }
